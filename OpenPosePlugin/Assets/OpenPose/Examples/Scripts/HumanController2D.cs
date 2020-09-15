a8772eaa1b7904a3b27b2eb98fac247b107442d3
shanemankiw <shanemankiw@sjtu.edu.cn>
2020-09-15 10:29:49 +0800

    adding 3D keypoint prediction

--- OpenPosePlugin/Assets/OpenPose/Examples/Scripts/HumanController2D.cs
+++ OpenPosePlugin/Assets/OpenPose/Examples/Scripts/HumanController2D.cs
@@ -15,6 +15,9 @@ namespace OpenPose.Example {
         public int PoseKeypointsCount = 25;
         public int HandKeypointsCount = 21;
         public int FaceKeypointsCount = 70;
+        public float HighestPoint = 999;
+        public float Biggest_Motion = -1;
+        public bool ifHighest = false;
 
         [SerializeField] RectTransform PoseParent;
         [SerializeField] RectTransform LHandParent;
@@ -34,6 +37,72 @@ namespace OpenPose.Example {
             DrawFace(ref datum, bodyIndex, scoreThres);
             DrawRectangles(ref datum, bodyIndex);
         }
+
+        public float CalMotionScore(ref OPDatum datum, ref OPDatum datum_old, int bodyIndex, int formerbodyindex,Vector2 motion_vector_temp){
+            
+            // for your protection
+            if (datum.poseKeypoints == null || bodyIndex >= datum.poseKeypoints.GetSize(0) || datum_old.poseKeypoints == null || formerbodyindex >= datum_old.poseKeypoints.GetSize(0)) {
+                PoseParent.gameObject.SetActive(false);
+                return 0;
+            }
+            // Pose
+            float temp = 0;
+            int i = 0;
+            for (int part = 0; part < poseJoints.Count; part++) {
+                // Joints overflow
+                if (part >= datum.poseKeypoints.GetSize(1) || part >= datum_old.poseKeypoints.GetSize(1)) {
+                    poseJoints[part].gameObject.SetActive(false);
+                    continue;
+                }
+                // Compare score
+                if (datum.poseKeypoints.Get(bodyIndex, part, 2) > 0.05 && datum_old.poseKeypoints.Get(formerbodyindex, part, 2) > 0.05) { // bug spot
+                    //poseJoints[part].gameObject.SetActive(true);
+                    Vector2 motion = new Vector2(datum.poseKeypoints.Get(bodyIndex, part, 0) - datum_old.poseKeypoints.Get(formerbodyindex, part, 0) - motion_vector_temp[0],
+                     datum.poseKeypoints.Get(bodyIndex, part, 1) - datum_old.poseKeypoints.Get(formerbodyindex, part, 1)-motion_vector_temp[1]);
+                    
+                    // Get the highest point in Y axis
+                    i = i + 1;
+                    temp = temp + motion[0] * motion[0] + motion[1] * motion[1];
+                    //poseJoints[part].localPosition = pos;
+                }
+            }
+            if (i != 0) {return temp/(i*(datum.poseKeypoints.Get(bodyIndex, 8, 2) - datum.poseKeypoints.Get(bodyIndex, 1, 2))*(datum.poseKeypoints.Get(bodyIndex, 8, 2) - datum.poseKeypoints.Get(bodyIndex, 1, 2)));} // try using i or not
+            else return 0;
+        }
+
+        public float CalSizeScore(ref OPDatum datum, int bodyIndex){
+            
+            // for your protection
+            if (datum.poseKeypoints == null || bodyIndex >= datum.poseKeypoints.GetSize(0)) {
+                PoseParent.gameObject.SetActive(false);
+                return 0;
+            }
+            // Pose
+            float lowest_point = -1;
+            float highest_point = 999;
+            for (int part = 0; part < poseJoints.Count; part++) {
+                // Joints overflow
+                if (part >= datum.poseKeypoints.GetSize(1)) {
+                    poseJoints[part].gameObject.SetActive(false);
+                    continue;
+                }
+                // 
+                if (datum.poseKeypoints.Get(bodyIndex, part, 2) > 0.05) {
+                    //poseJoints[part].gameObject.SetActive(true);
+                    if (datum.poseKeypoints.Get(bodyIndex, part, 1) > lowest_point){
+                        lowest_point = datum.poseKeypoints.Get(bodyIndex, part, 1);
+                    }
+
+                    if (datum.poseKeypoints.Get(bodyIndex, part, 1) < highest_point){
+                        highest_point = datum.poseKeypoints.Get(bodyIndex, part, 1);
+                    }
+                    
+                }
+            }
+            return Mathf.Max(0, lowest_point - highest_point);
+            
+        }
+
 
         private void DrawBody(ref OPDatum datum, int bodyIndex, float scoreThres){
             if (datum.poseKeypoints == null || bodyIndex >= datum.poseKeypoints.GetSize(0)) {
@@ -55,6 +124,10 @@ namespace OpenPose.Example {
                 } else {
                     poseJoints[part].gameObject.SetActive(true);
                     Vector3 pos = new Vector3(datum.poseKeypoints.Get(bodyIndex, part, 0), datum.poseKeypoints.Get(bodyIndex, part, 1), 0f);
+                    
+                    // Get the highest point in Y axis
+                    float temp = pos[1];
+                    if (temp < HighestPoint) HighestPoint = temp;
                     poseJoints[part].localPosition = pos;
                 }
             }
@@ -158,6 +231,29 @@ namespace OpenPose.Example {
             }
         }
 
+        /*
+        private void MotionScore(ref OPDatum datum, ref OPDatum datum_before, int bodyIndex, float scoreThres){
+            if (datum.poseKeypoints == null || bodyIndex >= datum.poseKeypoints.GetSize(0)) {
+                PoseParent.gameObject.SetActive(false);
+                return;
+            } else {
+                PoseParent.gameObject.SetActive(true);
+            }
+            // Pose
+            for (int part = 0; part < poseJoints.Count; part++) {
+                // Joints overflow
+                foreach (var human in humanContainer.GetComponentsInChildren<HumanController2D>()) {
+                    // When i >= no. of human, the human will be hidden
+                    human.DrawHuman(ref datum, i++, renderThreshold);
+                    
+                    if (human.HighestPoint < temp_highest_point) {
+                    temp_highest_point =  human.HighestPoint;
+                    spotlight_human_index = i - 1;
+                    }
+                }
+            }
+        }
+        */
         // Use this for initialization
         void Start() {
             InitJoints();
--- OpenPosePlugin/Assets/OpenPose/Examples/Scripts/OpenPoseUserScript.cs
+++ OpenPosePlugin/Assets/OpenPose/Examples/Scripts/OpenPoseUserScript.cs
@@ -20,22 +20,40 @@ namespace OpenPose.Example {
         [SerializeField] Text fpsText;
         [SerializeField] Text peopleText;
         [SerializeField] Text stateText;
+        [SerializeField] Text PointText;
 
         // Output
-        private OPDatum datum;
+        private OPDatum 
+        datum, // current data of the frame, the fifth datum
+        datum_1;
+
+        private float reference_torso_length = 382.0f;
+        private float real_torso_length = 0.8f; // m
+
+        private int num_of_people_1 = 0;
 
         // OpenPose settings
-        public ProducerType inputType = ProducerType.Webcam;
-        public string producerString = "-1";
+        public ProducerType inputType = ProducerType.Video;
+        public string producerString = "D://projects_in_class//openpose_unity_plugin//openpose-binary//examples//media//video.avi";
         public int maxPeople = -1;
         public float renderThreshold = 0.05f;
         public bool
             handEnabled = false,
             faceEnabled = false;
         public Vector2Int
-            netResolution = new Vector2Int(-1, 368),
-            handResolution = new Vector2Int(368, 368),
-            faceResolution = new Vector2Int(368, 368);
+            netResolution = new Vector2Int(-1, 16),
+            handResolution = new Vector2Int(16, 16),
+            faceResolution = new Vector2Int(16, 16);
+        
+        
+        public float alpha = 4; // the hyper-parameter we use to balance two types of scores
+
+        // What we are looking for.
+        public float
+        abs_x, // cordinate of x
+        abs_y, // cordinate of y
+        abs_z; // cordinate of z
+
         public void SetHandEnabled(bool enabled) { handEnabled = enabled; }
         public void SetFaceEnabled(bool enabled) { faceEnabled = enabled; }
         public void SetRenderThreshold(string s){float res; if (float.TryParse(s, out res)){renderThreshold = res;};}
@@ -53,7 +71,7 @@ namespace OpenPose.Example {
         }
 
         // Bg image
-        public bool renderBgImg = false;
+        public bool renderBgImg = true;
         public void ToggleRenderBgImg(){
             renderBgImg = !renderBgImg;
             bgImageRenderer.FadeInOut(renderBgImg);
@@ -179,10 +197,122 @@ namespace OpenPose.Example {
                     Instantiate(humanPrefab, humanContainer);
                 }
                 int i = 0;
+                int spotlight_human_index = -1;
+                //float temp_highest_point = 999;
+                float most_score = 0;
+                
+                foreach (var human in humanContainer.GetComponentsInChildren<HumanController2D>()) {
+                    // 2 ways
+                    // 1. motion score
+                    // 2. size score
+                    //}
+                    float motion_score = 0;
+                    float size_score = 0;
+                    bool if_datum_1_exist = false;
+                    if (frameCounter >= 2){
+                        //Motion score
+                        if(datum.poseKeypoints.GetSize(1) > 1 && datum.poseKeypoints != null && datum_1.poseKeypoints != null && !datum.poseKeypoints.Empty() && !datum_1.poseKeypoints.Empty()){
+                            if_datum_1_exist = true;
+                            Vector2 neck_current = new Vector2(datum.poseKeypoints.Get(i, 1, 0), datum.poseKeypoints.Get(i, 1, 1));
+                            float diff_min = 999;
+                            float diff_temp = 999;
+                            int formerID = -1;
+                            Vector2 motion_vector_temp = new Vector2(0, 0);
+
+                            //first, a simple reID process
+                            for (int index = 0; index < num_of_people_1;index++){
+                                if (datum_1.poseKeypoints == null || index >= datum_1.poseKeypoints.GetSize(0)) {
+                                    if_datum_1_exist = false;
+                                    break;
+                                    }
+                                Vector2 neck_4 = new Vector2(datum_1.poseKeypoints.Get(index, 1, 0),datum_1.poseKeypoints.Get(index, 1, 1));
+                                diff_temp = (neck_current - neck_4)[0]*(neck_current - neck_4)[0]  +  (neck_current - neck_4)[1]*(neck_current - neck_4)[1];             
+                                if(diff_temp < diff_min){
+                                    diff_min = diff_temp;
+                                    formerID = index;
+                                    motion_vector_temp[0] = (neck_current - neck_4)[0];
+                                    motion_vector_temp[1] = (neck_current - neck_4)[1];
+                                }
+                            }
+                            // calculate the absolute motions
+                            if (formerID != -1) {
+                                motion_score = human.CalMotionScore(ref datum, ref datum_1, i, formerID, motion_vector_temp);
+                            }
+                            
+                        }           
+                        else{
+                            motion_score = 0;
+                        }             
+                        
+                    }
+
+                    // size score
+                    if (datum.poseKeypoints != null){
+                        size_score = human.CalSizeScore(ref datum, i);
+                        size_score = size_score * size_score * alpha; // regularization
+                    }
+                    else{
+                        size_score = 0;
+                    }
+                    
+                    
+                    if (((motion_score + size_score) > most_score) && if_datum_1_exist){
+                            most_score = motion_score + size_score;
+                            spotlight_human_index = i;
+                        }
+                    i = i + 1;
+                    
+                }
+                if (frameCounter>=1) num_of_people_1 = i;
+                
+                /*
+                // get the spotlight_human_index
                 foreach (var human in humanContainer.GetComponentsInChildren<HumanController2D>()) {
                     // When i >= no. of human, the human will be hidden
                     human.DrawHuman(ref datum, i++, renderThreshold);
+                    
+                    if (human.HighestPoint < temp_highest_point) {
+                    temp_highest_point =  human.HighestPoint;
+                    spotlight_human_index = i - 1;
+                    }
+                }*/
+
+                // try to calculate the depth of the point
+                float torso_length = 0;
+                float reference_torso_length_scaled = reference_torso_length;
+                reference_torso_length_scaled = reference_torso_length * scale;
+                if (frameCounter >= 1){ // Already have five frames stored
+                    if (spotlight_human_index != -1 && datum.poseKeypoints != null) {
+                        if(datum.poseKeypoints.GetSize(1) > 8){
+                            Vector2 neck = new Vector2(datum.poseKeypoints.Get(spotlight_human_index, 1, 0),datum.poseKeypoints.Get(spotlight_human_index, 1, 1));
+                            Vector2 bottom = new Vector2(datum.poseKeypoints.Get(spotlight_human_index, 8, 0),datum.poseKeypoints.Get(spotlight_human_index, 8, 1));
+                            torso_length = bottom[1] - neck[1]; //assuming a vertical torso
+                            if (torso_length != 0){
+                                abs_z = reference_torso_length_scaled / torso_length; // unit: m
+                            }
+                            else{
+                                PointText.text = "Point:" + "unReferenced" + "," + "unReferenced" + "," + "unReferenced";
+                            }
+                            
+
+                            float f = reference_torso_length_scaled * real_torso_length;// f
+                            Vector2 c = new Vector2(screenSize.x/2, screenSize.y/2); // (c0,c1)
+                            Vector2 neck_normalized = neck - c;
+                            abs_x = abs_z * neck_normalized[0] / f;
+                            abs_y = abs_z * neck_normalized[1] / f;
+                            PointText.text = "Point:" + abs_x + "," + abs_y + "," + abs_z;
+                        }
+                        else{
+                            PointText.text = "Point:" + "nothing" + "," + "nothing" + "," + "nothing";
+                        }
+                        
+                        
+                    }
                 }
+
+                // update and store the whole scene
+                
+                datum_1 = datum;
 
                 // Update framerate in UI
                 frameTimeQueue.Enqueue(Time.time);
